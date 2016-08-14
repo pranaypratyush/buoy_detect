eb5bc704143695e1d960e3c6722653622f967fa8
Nightfury18 <srikanthreddypalem@gmail.com>
2016-08-14 13:10:35 +0530

    Fixed minor bugs

---
+++ region_growing/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/CIEDE2000.cpp.o"
+  "CMakeFiles/main.dir/src/grow.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
